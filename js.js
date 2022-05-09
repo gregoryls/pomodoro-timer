0d3a60ddcb39de42da5b9aca9efaddfd86d6aab7
Greg Schneider <greg53x@msn.com>
2022-05-09 12:33:34 -0600

    hook timer up to start button

--- js.js
+++ js.js
@@ -13,27 +13,29 @@ startButton = document.querySelector('.start');
 stopButton = document.querySelector('.stop');
 
 let min = 0;
-    let sec = 100;
+let sec = 100;
+
+
+startButton.addEventListener('click',()=> {
     stopButton.addEventListener('click',()=>{
         clearInterval(y);
-    })
-
-
-let y = setInterval(function() {
-    let minDisplay = Math.floor(sec/60);
-    let secDisplay = sec % 60;
-    if (secDisplay <10){
-        timerText.innerText = minDisplay+':0'+secDisplay;
-        document.title = minDisplay+':0'+secDisplay;
-    } else{
-        timerText.innerText = minDisplay+':'+secDisplay;
-        document.title = minDisplay+':'+secDisplay;
-    }
+    });
+    let y = setInterval(function() {
+        let minDisplay = Math.floor(sec/60);
+        let secDisplay = sec % 60;
+        if (secDisplay <10){
+            timerText.innerText = minDisplay+':0'+secDisplay;
+            document.title = minDisplay+':0'+secDisplay;
+        } else{
+            timerText.innerText = minDisplay+':'+secDisplay;
+            document.title = minDisplay+':'+secDisplay;
+        }
+        
+        sec -= 1;
     
-    sec -= 1;
-
-    if (sec<0){
-        clearInterval(y);
-    }
-
-},1000)
+        if (sec<0){
+            clearInterval(y);
+        }
+    
+    },1000)
+})
