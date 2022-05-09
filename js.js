95e8afe34eb33c1a592a53cfa0fae18c739f9f1a
Greg Schneider <greg53x@msn.com>
2022-05-08 23:27:33 -0600

    stop button halts timer countdown

--- js.js
+++ js.js
@@ -7,20 +7,27 @@ function getCurrentTime(){
 let currentTime;
 
 
+
 timerText = document.querySelector('.timer');
 startButton = document.querySelector('.start');
+stopButton = document.querySelector('.stop');
 startButton.addEventListener('click',() =>{
-    let timerLength = 50*1000;
+    stopButton.addEventListener('click',()=>{
+        clearInterval(x);
+    })
+    let timerLength = 25*60*1000+1000; //+1000 so it displays XX:59 first instead of XX:58
+    timerText.innerText = '25:00';
     getCurrentTime();    
     let timerEnd = currentTime + timerLength;
     let x = setInterval(function() {
         getCurrentTime();
-        //25 minutes converted to milliseconds
-        
+
         let timerDistance = timerEnd - currentTime;
         console.log(timerDistance)
         let minutes = Math.floor((timerDistance % (1000 * 60 * 60)) / (1000 * 60));
         let seconds = Math.floor((timerDistance % (1000 * 60)) / 1000);
+        if (seconds<10) seconds = '0'+seconds;
+        if (minutes<10) minutes = '0'+minutes;
         timerText.innerText = minutes + ':' + seconds;
         if (timerDistance<0){
             clearInterval(x);
