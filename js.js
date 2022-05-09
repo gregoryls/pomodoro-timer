6c3a940481de7a8c1b015d87be3624dd9dc92f5a
Greg Schneider <greg53x@msn.com>
2022-05-09 12:17:14 -0600

    change timer to use seconds as input

--- js.js
+++ js.js
@@ -11,47 +11,59 @@ let currentTime;
 timerText = document.querySelector('.timer');
 startButton = document.querySelector('.start');
 stopButton = document.querySelector('.stop');
-startButton.addEventListener('click',() =>{
+// startButton.addEventListener('click',() =>{
+//     stopButton.addEventListener('click',()=>{
+//         clearInterval(x);
+//     })
+//     let timerLength = 25*60*1000+1000; //+1000 so it displays XX:59 first instead of XX:58
+//     timerText.innerText = '25:00';
+//     getCurrentTime();    
+//     let timerEnd = currentTime + timerLength;
+//     let x = setInterval(function() {
+//         getCurrentTime();
+
+//         let timerDistance = timerEnd - currentTime;
+//         console.log(timerDistance)
+//         let minutes = Math.floor((timerDistance % (1000 * 60 * 60)) / (1000 * 60));
+//         let seconds = Math.floor((timerDistance % (1000 * 60)) / 1000);
+//         if (seconds<10) seconds = '0'+seconds;
+//         if (minutes<10) minutes = '0'+minutes;
+//         timerText.innerText = minutes + ':' + seconds;
+//         if (timerDistance<0){
+//             clearInterval(x);
+//             timerText.innerText = '00:00';
+//         }
+//     },1000);
+    
+    
+// });
+let min = 0;
+    let sec = 65;
     stopButton.addEventListener('click',()=>{
-        clearInterval(x);
-    })
-    let timerLength = 25*60*1000+1000; //+1000 so it displays XX:59 first instead of XX:58
-    timerText.innerText = '25:00';
-    getCurrentTime();    
-    let timerEnd = currentTime + timerLength;
-    let x = setInterval(function() {
-        getCurrentTime();
-
-        let timerDistance = timerEnd - currentTime;
-        console.log(timerDistance)
-        let minutes = Math.floor((timerDistance % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((timerDistance % (1000 * 60)) / 1000);
-        if (seconds<10) seconds = '0'+seconds;
-        if (minutes<10) minutes = '0'+minutes;
-        timerText.innerText = minutes + ':' + seconds;
-        if (timerDistance<0){
-            clearInterval(x);
-            timerText.innerText = '00:00';
-        }
-    },1000);
-    
-    
-});
-let min = 10;
-    let sec = 0;
-let y = setInterval(function() {
-    if (min < 10 && sec < 10) timerText.innerText = '0'+min + ':' + '0'+sec;
-    else if (min<10)timerText.innerText = '0'+min + ':' + sec;
-    else if (sec<10)timerText.innerText = min + ':' + '0'+sec;
-    else timerText.innerText = min + ':' + sec;
-    
-    sec -= 1;
-    if (sec<0) {
-        min -= 1;
-        sec = 59;
-    }    
-    if (min == 0 && sec == 0){
         clearInterval(y);
-    }
+    })
+// let y = setInterval(function() {
+//     if (min < 10 && sec < 10) timerText.innerText = '0'+min + ':' + '0'+sec;
+//     else if (min<10)timerText.innerText = '0'+min + ':' + sec;
+//     else if (sec<10)timerText.innerText = min + ':' + '0'+sec;
+//     else timerText.innerText = min + ':' + sec;
+    
+//     sec -= 1;
+//     if (sec<0) {
+//         min -= 1;
+//         sec = 59;
+//     }    
+//     if (min == 0 && sec == 0){
+//         clearInterval(y);
+        
+//     }
 
-},1000);
+// },1000);
+
+let y = setInterval(function() {
+    let minDisplay = Math.floor(sec/60);
+    let secDisplay = sec % 60;
+    timerText.innerText = minDisplay+':'+secDisplay;
+    sec -= 1;
+
+},1000)
