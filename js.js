5a75d018192d786a5732c41f00986d77cd890bbe
Greg Schneider <greg53x@msn.com>
2022-05-09 12:31:38 -0600

    remove unused minute-based timer code

--- js.js
+++ js.js
@@ -17,23 +17,7 @@ let min = 0;
     stopButton.addEventListener('click',()=>{
         clearInterval(y);
     })
-// let y = setInterval(function() {
-//     if (min < 10 && sec < 10) timerText.innerText = '0'+min + ':' + '0'+sec;
-//     else if (min<10)timerText.innerText = '0'+min + ':' + sec;
-//     else if (sec<10)timerText.innerText = min + ':' + '0'+sec;
-//     else timerText.innerText = min + ':' + sec;
-    
-//     sec -= 1;
-//     if (sec<0) {
-//         min -= 1;
-//         sec = 59;
-//     }    
-//     if (min == 0 && sec == 0){
-//         clearInterval(y);
-        
-//     }
 
-// },1000);
 
 let y = setInterval(function() {
     let minDisplay = Math.floor(sec/60);
