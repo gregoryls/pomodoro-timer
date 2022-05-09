01cf190b9e5290b4a5811974e7aed595b64d3380
Greg Schneider <greg53x@msn.com>
2022-05-09 12:31:01 -0600

    remove unused date-based timer code

--- js.js
+++ js.js
@@ -11,32 +11,7 @@ let currentTime;
 timerText = document.querySelector('.timer');
 startButton = document.querySelector('.start');
 stopButton = document.querySelector('.stop');
-// startButton.addEventListener('click',() =>{
-//     stopButton.addEventListener('click',()=>{
-//         clearInterval(x);
-//     })
-//     let timerLength = 25*60*1000+1000; //+1000 so it displays XX:59 first instead of XX:58
-//     timerText.innerText = '25:00';
-//     getCurrentTime();    
-//     let timerEnd = currentTime + timerLength;
-//     let x = setInterval(function() {
-//         getCurrentTime();
 
-//         let timerDistance = timerEnd - currentTime;
-//         console.log(timerDistance)
-//         let minutes = Math.floor((timerDistance % (1000 * 60 * 60)) / (1000 * 60));
-//         let seconds = Math.floor((timerDistance % (1000 * 60)) / 1000);
-//         if (seconds<10) seconds = '0'+seconds;
-//         if (minutes<10) minutes = '0'+minutes;
-//         timerText.innerText = minutes + ':' + seconds;
-//         if (timerDistance<0){
-//             clearInterval(x);
-//             timerText.innerText = '00:00';
-//         }
-//     },1000);
-    
-    
-// });
 let min = 0;
     let sec = 100;
     stopButton.addEventListener('click',()=>{
